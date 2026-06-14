4981be4cc4cfc99ee2d920fb3cf11c7d4243baf4
LukaRic <lukajacobsen1712@gmail.com>
2026-06-14 10:13:46 +0200

    Removed unused imports

--- src/user_commands/mineshaft.js
+++ src/user_commands/mineshaft.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, InteractionContextType, EmbedBuilder, MessageFlags, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType} = require('discord.js');
 
 const { Colors, formatBalance, buildAuthor, handleInteractionError } = require('../utils/standards.js');
-const { createInsufficientMoneyEmbed, createTimedOutEmbed, createIlligalInteractionEmbed } = require('../utils/standard_embeds.js');
+const { createInsufficientMoneyEmbed, createIlligalInteractionEmbed } = require('../utils/standard_embeds.js');
 
 /**
  * Generates a right-skewed random hourly rate.
