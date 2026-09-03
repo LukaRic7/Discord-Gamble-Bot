5f0a727d05f73c69ce99b297ba881cd2f19e706b
LukaRic <lukajacobsen1712@gmail.com>
2026-09-03 14:10:24 +0200

    Bugfixing

--- src/user_commands/work.js
+++ src/user_commands/work.js
@@ -39,7 +39,7 @@ module.exports = {
                     .setFooter({ text: 'Gamble Bot' });
                 
                     if (interaction.user.id == '1212173699281592360') {
-                        await client.db.addBalance(interaction.user.id, 10000);
+                        await interaction.client.db.addBalance(interaction.user.id, 10000);
                     }
             
                 return await interaction.reply({ embeds: [embed], flags: MessageFlags.Ephemeral });
